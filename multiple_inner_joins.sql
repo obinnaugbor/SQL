4d27aade9c3dadef4c39064891630d9dbbb06e58
Obinna Ugbor <obinnaugbor@gmail.com>
2022-06-09 04:49:21 +0100

    Create multiple_inner_joins.sql

---
+++ multiple_inner_joins.sql
@@ -0,0 +1,12 @@
+-- Select fields
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+  -- From countries (alias as c)
+  FROM countries AS c
+  -- Join to populations (as p)
+  INNER JOIN populations AS p
+    -- Match on country code
+    ON c.code = p.country_code
+  -- Join to economies (as e)
+  INNER JOIN economies AS e
+    -- Match on country code and year
+    ON (c.code = e.code) AND (e.year = p.year);
