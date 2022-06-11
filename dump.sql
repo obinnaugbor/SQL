09bbcfee4b0304ea4c7861ddf612df03529c2498
Obinna Ugbor <obinnaugbor@gmail.com>
2022-06-11 06:18:18 +0100

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -10,3 +10,12 @@ select city, length(city)
 from station
 order by length(city) desc
 limit 1;
+
+
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE (CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U');
